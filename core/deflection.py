ccbe5ac4e866b8fae7e9c6fa6cb879273354df1e
Eharriman <eharriman@elhnet.com>
2026-04-05 11:45:17 -0400

    missing return statement in beta calc.

--- core/deflection.py
+++ core/deflection.py
@@ -7,8 +7,10 @@ def generated_pointLensDeflection(theta_vec, theta_einstein):
     return (theta_einstein ** 2 / norm_sqr) * theta_vec
 
 
-def map_source_to_lens(theta_vec, theta_einstein):
+def map_theta_to_beta(theta_vec, theta_einstein):
     
     alpha = generated_pointLensDeflection(theta_vec, theta_einstein)
 
     beta = theta_vec - alpha
+
+    return beta
