f7ef33d92d892b91bffae7c2b8aef601ed0c3d66
Eharriman <eharriman@elhnet.com>
2026-04-05 11:50:45 -0400

    testing scalr to vector mapping

---
+++ tests/test_lens_equation.py
@@ -0,0 +1,14 @@
+from core.lens_equation import image_positions
+from core.deflection import map_theta_to_beta
+import numpy as np
+
+theta_E = 1.0
+beta_test = 0.5
+
+tp, tm = image_positions(beta_test, theta_E)
+
+theta_vec = np.array([tp, 0.0])
+beta_vec = map_theta_to_beta(theta_vec, theta_E)
+
+print(f"The scalar beta is : {beta_test}")
+print(f"The mapped vector beta X: {beta_vec[0]}")
