2f8dfa23c470bbb1b6eb14a2907c03ee01739333
Eharriman <eharriman@elhnet.com>
2026-04-05 08:55:14 -0400

    constants

---
+++ core/constants.py
@@ -0,0 +1,30 @@
+# All physical constants required for lensing and other calculations
+
+from scipy import constants
+
+# Fundamental constants
+c = constants.c
+G = constants.G
+h_bar = constants.hbar
+
+
+# Distances
+
+ly = 9.46e17 # in cm
+psc = 3.26 * ly
+
+
+# Earth
+au = 1.50e13
+M_earth = 5.97e27
+R_earth = 6.38e8
+
+# Sun
+M_sun = 1.99e33
+R_sun = 6.69e10
+
+
+#print(ly * 2)
+#print(psc)
+
+
