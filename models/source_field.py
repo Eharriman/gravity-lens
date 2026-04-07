cadfb3a652b620dcfd35750ac9463f5912ffad98
Eharriman <eharriman@elhnet.com>
2026-04-06 21:39:08 -0400

    testing sersic

--- models/source_field.py
+++ models/source_field.py
@@ -7,3 +7,18 @@ def gaussian_source(beta_grid, center=(0.6, 0.0), sigma=0.08, amplitude=1.0):
 
     r2 = (bx - cx)**2 + (by - cy)**2
     return amplitude * np.exp(-r2 / (2 * sigma**2))
+
+
+def sersic_source(beta_grid, center=(0.6, 0.0), amplitude=1.0, R_eff=0.2, n_sersic=1.0):
+
+    bx = beta_grid[..., 0]
+    by = beta_grid[..., 1]
+    cx, cy = center
+
+    r = np.sqrt((bx - cx)**2 + (by - cy)**2)
+    
+    bn = 1.9992 * n_sersic - 0.3271
+    
+    # 3. Sersic formula
+    exponent = -bn * ((r / R_eff)**(1 / n_sersic) - 1)
+    return amplitude * np.exp(exponent)
--- simulations/field_lensing.py
+++ simulations/field_lensing.py
@@ -1,5 +1,5 @@
 import numpy as np
-from models.source_field import gaussian_source
+from models.source_field import gaussian_source, sersic_source
 from core.lens_mapping import generate_theta_grid, map_theta_to_beta
 
 def generate_lensed_field(theta_max, n, theta_einstein, 
@@ -8,17 +8,31 @@ def generate_lensed_field(theta_max, n, theta_einstein,
     theta_grid = generate_theta_grid(theta_max, n)
     beta_grid = map_theta_to_beta(theta_grid, theta_einstein)
 
+    # Beta is unlensed?
+    '''
     unlensed_image = gaussian_source(
-        beta_grid,
+        theta_grid,
         center=source_center,
         sigma=source_sigma
     )
 
     lensed_image = gaussian_source(
-        theta_grid,
+        beta_grid,
         center=source_center,
         sigma=source_sigma
+    ) 
+    '''
+
+    unlensed_image = sersic_source(
+        theta_grid,
+        center=source_center
     )
+
+    lensed_image = sersic_source(
+        beta_grid,
+        center=source_center
+    )
+
 
     return theta_grid, beta_grid, unlensed_image, lensed_image
 
