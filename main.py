59671bd7847a5fabf84af49921b6df6e1c62c960
Eharriman <eharriman@elhnet.com>
2026-04-07 14:53:46 -0400

    working PoC

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from simulations.field_lensing import generate_lensed_field
 from render.field_render import plot_lensed_comparison
+import random
+
 
 if __name__ == "__main__":
     theta_max = 2.0
+    theta_max = 3.4
     n = 500
     theta_einstein = 0.5
 
@@ -13,5 +16,7 @@ if __name__ == "__main__":
         source_center=(0.4, 0.3),
         source_sigma=0.08
     )
+
+
 
     plot_lensed_comparison(unlensed_image, lensed_image, theta_max)
