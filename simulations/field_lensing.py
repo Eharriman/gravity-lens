0d878a6ad49b518ec7d618ec093da2a64a0d3761
Eharriman <eharriman@elhnet.com>
2026-04-07 08:01:12 -0400

    testing different values

--- simulations/field_lensing.py
+++ simulations/field_lensing.py
@@ -3,7 +3,7 @@ from models.source_field import gaussian_source, sersic_source
 from core.lens_mapping import generate_theta_grid, map_theta_to_beta
 
 def generate_lensed_field(theta_max, n, theta_einstein, 
-                           source_center=(0.0,0.0), source_sigma=0.08):
+                           source_center=(0.0,0.0), source_sigma=0.00):
     
     theta_grid = generate_theta_grid(theta_max, n)
     beta_grid = map_theta_to_beta(theta_grid, theta_einstein)
@@ -22,7 +22,7 @@ def generate_lensed_field(theta_max, n, theta_einstein,
         sigma=source_sigma
     ) 
     '''
-
+    
     unlensed_image = sersic_source(
         theta_grid,
         center=source_center
@@ -32,7 +32,7 @@ def generate_lensed_field(theta_max, n, theta_einstein,
         beta_grid,
         center=source_center
     )
-
+    
 
     return theta_grid, beta_grid, unlensed_image, lensed_image
 
