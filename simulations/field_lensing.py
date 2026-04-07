914d14702d8a66e1db0f44fd34af3189c608de87
Eharriman <eharriman@elhnet.com>
2026-04-06 21:27:43 -0400

    typo

--- simulations/field_lensing.py
+++ simulations/field_lensing.py
@@ -2,7 +2,7 @@ import numpy as np
 from models.source_field import gaussian_source
 from core.lens_mapping import generate_theta_grid, map_theta_to_beta
 
-def generated_lensed_field(theta_max, n, theta_einstein, 
+def generate_lensed_field(theta_max, n, theta_einstein, 
                            source_center=(0.0,0.0), source_sigma=0.08):
     
     theta_grid = generate_theta_grid(theta_max, n)
