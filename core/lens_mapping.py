871bdef8fb4833a4f438c6dc21590dc6004d3f06
Eharriman <eharriman@elhnet.com>
2026-04-06 21:05:57 -0400

    mesh grid for lens map

--- core/lens_mapping.py
+++ core/lens_mapping.py
@@ -10,3 +10,12 @@ def map_theta_to_beta(theta_vec, theta_einstein):
     beta = theta_vec - alpha
 
     return beta
+
+
+def generate_theta_grid(theta_max, n):
+    x = np.linspace(-theta_max, theta_max, n)
+    y = np.linspace(-theta_max, theta_max, n)
+    #grid = np.stack(np.meshgrid(x,y), axis=-1)
+
+    X, Y = np.meshgrid(x, y, indexing="xy")
+    return np.stack((X,Y), axis=-1)
