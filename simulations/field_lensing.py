372b3d3213b6603470b58a92e0d3b3229c315396
Eharriman <eharriman@elhnet.com>
2026-04-10 18:07:14 -0400

    field lensing using jacobian methods

--- simulations/field_lensing.py
+++ simulations/field_lensing.py
@@ -1,5 +1,10 @@
 import numpy as np
 from core.lens_mapping import generate_theta_grid, map_theta_to_beta
+from core.jacobian import (
+    point_lens_det_jacobian,
+    point_lens_magnfication_field,
+    point_lens_eigenvalues
+)
 from models.source_field import source_field
 
 def generate_lensed_field(theta_max, n, theta_einstein, 
@@ -12,5 +17,18 @@ def generate_lensed_field(theta_max, n, theta_einstein,
     
     lensed_image = source_field(beta_grid, source_list)
 
-    return theta_grid, beta_grid, unlensed_image, lensed_image
+    # Equivalent formulation using Jacobian derivation
+    det_jacobian = point_lens_det_jacobian(theta_grid, theta_einstein)
+    magnification = point_lens_magnfication_field(theta_grid, theta_einstein)
+    lambda_r, lambda_t = point_lens_eigenvalues(theta_grid, theta_einstein)
 
+    return {
+        "theta_grid": theta_grid,
+        "beta_grid": beta_grid,
+        "unlensed_image": unlensed_image,
+        "lensed_image": lensed_image,
+        "det_jacobian": det_jacobian,
+        "magnification": magnification,
+        "lambda_r": lambda_r,
+        "lambda_t": lambda_t,
+    }
