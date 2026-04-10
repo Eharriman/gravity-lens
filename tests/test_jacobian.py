df5a36e6c075ee497f09882c0a34ddf884a615e0
Eharriman <eharriman@elhnet.com>
2026-04-10 18:02:23 -0400

    eigen values tested using determinant comparison

--- tests/test_jacobian.py
+++ tests/test_jacobian.py
@@ -1,5 +1,5 @@
 import numpy as np
-from core.jacobian import point_lens_jacobian
+from core.jacobian import point_lens_jacobian, point_lens_det_jacobian, point_lens_eigenvalues, point_lens_magnfication_field
 
 def test_jacobian_symmetry():
     theta = np.asarray([1.2,2.4])
@@ -7,3 +7,12 @@ def test_jacobian_symmetry():
     jac = point_lens_jacobian(theta, 0.5)
 
     assert np.isclose(jac[0,1], jac[1,0], atol=1e-12)
+
+
+def  test_jacobian_eigenvalues():
+    theta = np.asarray([1.2,2.4])
+    jac = point_lens_jacobian(theta, 0.5)
+    det = point_lens_det_jacobian(theta, 0.5)
+    lambda_r, lambda_t = point_lens_eigenvalues(theta, 0.5)
+
+    assert np.isclose(det, lambda_r*lambda_t, atol=1e-12)
