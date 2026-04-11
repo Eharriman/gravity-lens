9db1a1faf43777f581b7a74ec8b41caf4350ff0e
Eharriman <eharriman@elhnet.com>
2026-04-10 23:50:44 -0400

    work and changing cmap

--- render/field_render.py
+++ render/field_render.py
@@ -6,12 +6,12 @@ def plot_lensed_comparison(unlensed_image, lensed_image, theta_max):
 
     fig, axes = plt.subplots(1, 2, figsize=(10, 4))
 
-    axes[0].imshow(unlensed_image, origin="lower", extent=extent)
+    axes[0].imshow(unlensed_image, origin="lower", extent=extent, vmin=0, vmax=4.0, cmap="inferno")
     axes[0].set_title("Unlensed Source")
     axes[0].set_xlabel(r"Angular displacement: $\theta_x$")
     axes[0].set_ylabel(r"Angular displacement: $\theta_y$")
 
-    axes[1].imshow(lensed_image, origin="lower", extent=extent)
+    axes[1].imshow(lensed_image, origin="lower", extent=extent, vmin=0, vmax=4.0, cmap="inferno")
     axes[1].set_title("Lensed Image")
     axes[1].set_xlabel(r"Angular displacement: $\theta_x$")
     axes[1].set_ylabel(r"Angular displacement: $\theta_y$")
