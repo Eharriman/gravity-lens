ca72e00cceb0253b552444c2bba8dcd633363f0d
Eharriman <eharriman@elhnet.com>
2026-04-14 10:11:58 -0400

    fig save method

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -10,3 +10,14 @@ def generate_filename(prefix="lens", ext="png", tag=None):
     if tag:
         return f"{prefix}_{tag}_{timestamp}.{ext}"
     return f"{prefix}_{timestamp}.{ext}"
+
+
+def save_figure(fig, filename=None, prefix="lens", tag=None, dpi=150):
+    
+    if filename is None:
+        filename = generate_filename(prefix=prefix,tag=tag)
+
+    path = OUTPUT_DIR / filename
+
+    fig.savefig(path, dpi=dpi, bbox_inches="tight")
+    print("[Output] Simulation figure saved to -> {path}")
