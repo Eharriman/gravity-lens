60959338fc5ac23415262950895f68ab7512b696
Eharriman <eharriman@elhnet.com>
2026-04-14 12:14:19 -0400

    refactor lensed comparison with save figure in image render

--- render/image_render.py
+++ render/image_render.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+from utils.io_utils import save_figure
 
 
-def plot_image_comparison(original, lensed):
+def plot_image_comparison(original, lensed, save=False, tag=None):
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
     axes[0].imshow(original, cmap="gray")
@@ -11,6 +12,9 @@ def plot_image_comparison(original, lensed):
     axes[1].imshow(lensed, cmap="gray")
     axes[1].set_title("Lensed Image")
     axes[1].axis("off")
+
+    if save:
+        save_figure(fig, prefix="field_lens", tag=tag)
 
     plt.tight_layout()
     plt.show()
