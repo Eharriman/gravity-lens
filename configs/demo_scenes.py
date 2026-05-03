ed8379231dda1bb4330b5b2205062356b55489a7
Eharriman <eharriman@elhnet.com>
2026-05-03 14:42:27 -0400

    simple scene build

--- configs/demo_scenes.py
+++ configs/demo_scenes.py
@@ -5,3 +5,29 @@ def build_demo_source_list():
         {"type": "gaussian_elliptical", "center": (0.9, 0.6), "sigma_major": 0.14, "sigma_minor": 0.05, "angle": 0.3, "amplitude": 1.5},
         {"type": "sersic_source", "center": (-0.4, -0.3), "R_eff": 0.12, "n_sersic": 1.2, "amplitude": 1.0},
     ]
+
+
+def build_simple_source_list():
+    return [
+        {
+            "type": "gaussian_circular",
+            "center": (0.6, 0.2),
+            "sigma": 0.06,
+            "amplitude": 2.2,
+        },
+        {
+            "type": "gaussian_elliptical",
+            "center": (-0.5, 0.4),
+            "sigma_major": 0.14,
+            "sigma_minor": 0.05,
+            "angle": 0.6,
+            "amplitude": 5.5,
+        },
+        {
+            "type": "sersic_source",
+            "center": (-0.3, -0.4),
+            "amplitude": 1.0,
+            "R_eff": 0.12,
+            "n_sersic": 1.2,
+        },
+    ]
