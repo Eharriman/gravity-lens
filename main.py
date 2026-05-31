8eeff6fdecbc0b94ad59a8c47ef735c2dac390ee
Eharriman <eharriman@elhnet.com>
2026-05-31 12:15:20 -0400

    removed old generated pop. demo from main

--- main.py
+++ main.py
@@ -73,43 +73,6 @@ def apply_parse_config(config, args):
 
     return config
 
-def generate_demo_sourcelist():
-    return [
-        # --- Compact Gaussian sources (stars / compact galaxies) ---
-    {"type": "gaussian_circular", "center": (0.6, 0.2), "sigma": 0.05, "amplitude": 1.5},
-    {"type": "gaussian_circular", "center": (-0.8, 0.5), "sigma": 0.04, "amplitude": 1.2},
-    {"type": "gaussian_circular", "center": (0.3, -0.7), "sigma": 0.06, "amplitude": 1.8},
-    {"type": "gaussian_circular", "center": (-0.2, 0.9), "sigma": 0.05, "amplitude": 1.0},
-    {"type": "gaussian_circular", "center": (1.1, -0.4), "sigma": 0.03, "amplitude": 0.8},
-    {"type": "gaussian_circular", "center": (-1.2, -0.6), "sigma": 0.04, "amplitude": 1.3},
-
-    # --- Elliptical galaxies ---
-    {"type": "gaussian_elliptical", "center": (0.9, 0.6), "sigma_major": 0.14, "sigma_minor": 0.05, "angle": 0.3, "amplitude": 1.5},
-    {"type": "gaussian_elliptical", "center": (-0.9, 0.1), "sigma_major": 0.12, "sigma_minor": 0.04, "angle": 1.0, "amplitude": 1.3},
-    {"type": "gaussian_elliptical", "center": (0.2, 1.0), "sigma_major": 0.10, "sigma_minor": 0.03, "angle": 0.7, "amplitude": 1.1},
-    {"type": "gaussian_elliptical", "center": (-0.6, -1.1), "sigma_major": 0.15, "sigma_minor": 0.06, "angle": 0.2, "amplitude": 1.6},
-    {"type": "gaussian_elliptical", "center": (1.2, 0.2), "sigma_major": 0.11, "sigma_minor": 0.05, "angle": 1.3, "amplitude": 1.2},
-    {"type": "gaussian_elliptical", "center": (-1.0, -0.2), "sigma_major": 0.13, "sigma_minor": 0.04, "angle": 0.5, "amplitude": 1.4},
-
-    # --- Sérsic galaxies (diffuse structure) ---
-    {"type": "sersic_source", "center": (-0.4, -0.3), "R_eff": 0.12, "n_sersic": 1.2, "amplitude": 1.0},
-    {"type": "sersic_source", "center": (0.5, -0.9), "R_eff": 0.18, "n_sersic": 0.8, "amplitude": 1.2},
-    {"type": "sersic_source", "center": (-1.1, 0.8), "R_eff": 0.10, "n_sersic": 2.0, "amplitude": 1.1},
-    {"type": "sersic_source", "center": (1.0, 0.9), "R_eff": 0.14, "n_sersic": 1.5, "amplitude": 1.3},
-    {"type": "sersic_source", "center": (-0.2, -1.2), "R_eff": 0.16, "n_sersic": 0.9, "amplitude": 1.2},
-
-    # --- Near-lens interesting sources (strong lensing region) ---
-    {"type": "gaussian_circular", "center": (0.25, 0.05), "sigma": 0.05, "amplitude": 2.0},
-    {"type": "gaussian_elliptical", "center": (-0.3, 0.1), "sigma_major": 0.12, "sigma_minor": 0.04, "angle": 0.4, "amplitude": 1.8},
-    {"type": "sersic_source", "center": (0.1, -0.2), "R_eff": 0.10, "n_sersic": 1.0, "amplitude": 1.5},
-
-    # --- Peripheral faint background ---
-    {"type": "gaussian_circular", "center": (1.4, -1.2), "sigma": 0.05, "amplitude": 0.7},
-    {"type": "gaussian_circular", "center": (-1.5, 1.2), "sigma": 0.04, "amplitude": 0.6},
-    {"type": "gaussian_elliptical", "center": (1.3, 1.3), "sigma_major": 0.10, "sigma_minor": 0.04, "angle": 0.9, "amplitude": 0.8},
-    {"type": "sersic_source", "center": (-1.4, -1.3), "R_eff": 0.12, "n_sersic": 1.3, "amplitude": 0.9}
-    ]
-
 
 def run_source_list_sim(config):
     #source_list = build_demo_source_list()
